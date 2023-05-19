c4e57c552778ddfd8b459f1eb3525d1301b615b5
ANGEL GOMEZ-HUMMEL <gomezhummel@plu.edu>
2023-05-18 17:07:40 -0700

    created sql query and added table with styling and custom scrollbar

--- app.py
+++ app.py
@@ -10,7 +10,23 @@ database_path = 'nba.sqlite'
 # example starting page
 @app.route("/")
 def start_page():
-    return render_template("welcome.html")
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    # Execute the query
+    query = "SELECT player_name, position, height_wo_shoes_ft_in, height_w_shoes_ft_in, weight, wingspan_ft_in, standing_reach_ft_in, body_fat_pct, hand_length, hand_width, standing_vertical_leap, max_vertical_leap, lane_agility_time, modified_lane_agility_time, three_quarter_sprint FROM draft_combine_stats"
+    cursor.execute(query)
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Close the database connection
+    cursor.close()
+    conn.close()
+
+    # Pass the results to the template for rendering
+    return render_template("sets.html", results=results)
+
 
 
 if __name__ == '__main__':
